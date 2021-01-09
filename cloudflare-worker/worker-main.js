fbb4592d4c6008186eecd127343f490677007a17
Ahmet OZER <git@ahmetozer.org>
2021-01-09 19:10:52 +0300

    gatherResponse

--- cloudflare-worker/worker-main.js
+++ cloudflare-worker/worker-main.js
@@ -1,3 +1,20 @@
+// Configure with your own settings
+const domainZone = ""
+const authToken = ""
+const otherExpressions = ""
+
+async function handleRequest() {
+   const init = {
+      method: 'PUT',
+     headers: {
+         "content-type": "application/json;charset=UTF-8",
+        "Authorization":"Bearer "+authToken,
+     },
+   }
+   const response = await fetch("https://api.cloudflare.com/client/v4/zones/"+domainZone+"/firewall/rules", init)
+   const results = await gatherResponse(response)
+   return new Response(results, init)
+ }
 
 
 /*
@@ -11,4 +28,27 @@ function randomString(length) {
        result += characters.charAt(Math.floor(Math.random() * randomStringCharacters.length));
     }
     return result;
+}
+
+function newExpression(expression) {
+   if (otherExpressions != undefined && otherExpressions != null && otherExpressions != "") {
+      expression = expression + " or "
+   }
+   return expression
+}
+async function gatherResponse(response) {
+   const { headers } = response
+   const contentType = headers.get("content-type") || ""
+   if (contentType.includes("application/json")) {
+     return JSON.stringify(await response.json())
+   }
+   else if (contentType.includes("application/text")) {
+     return await response.text()
+   }
+   else if (contentType.includes("text/html")) {
+     return await response.text()
+   }
+   else {
+     return await response.text()
+   }
  }
